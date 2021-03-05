9dec50216871f050b7e9a8c3ddcc074b2a437af0
kipkoalex <kipkoalex@yahoo.com>
2021-03-05 15:59:59 +0200

    Update tubePack.py

--- tubePack.py
+++ tubePack.py
@@ -195,18 +195,22 @@ def train(model):
     # no need to train all layers, just the heads should do it.
     print("Training network heads")
     augmentation = imgaug.augmenters.Sequential([
-        imgaug.augmenters.Fliplr(0.5),
-        imgaug.augmenters.Flipud(0.2),
-        # imgaug.augmenters.GaussianBlur(sigma=(0.0, 5.0))
-        imgaug.augmenters.Sometimes(0.5, imgaug.augmenters.GaussianBlur(sigma=(0, 0.5))),
-        imgaug.augmenters.LinearContrast((0.75, 1.5)),
-        imgaug.augmenters.Affine(
-            # scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-            # translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-            rotate=(-5, 5),
-            # shear=(-8, 8)
-        )
-    ])
+        imgaug.augmenters.Fliplr(0.5),  # horizontally flip 50% of the images
+        imgaug.augmenters.Flipud(0.5),  # horizontally flip 50% of the images
+        imgaug.augmenters.sometimes(imgaug.augmenters.CropAndPad(
+            percent=(-0.05, 0.1),
+            pad_mode=imgaug.ALL,
+            pad_cval=(0, 255)
+        )),
+        imgaug.augmenters.sometimes(imgaug.augmenters.Affine(
+            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},  # scale images to 80-120% of their size, individually per axis
+            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent (per axis)
+            rotate=(-45, 45),  # rotate by -45 to +45 degrees
+            shear=(-16, 16),  # shear by -16 to +16 degrees
+            order=[0, 1],  # use nearest neighbour or bilinear interpolation (fast)
+            cval=(0, 255),  # if mode is constant, use a cval between 0 and 255
+            mode=imgaug.ALL  # use any of scikit-image's warping modes (see 2nd image from the top for examples)
+        )), ])
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
                 epochs=30, augmentation=augmentation,
